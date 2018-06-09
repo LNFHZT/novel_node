415d74df4ff69bf9265fded61eeea8d8fdae7f5d
LNFHZT <LNFHZT@163.com>
2018-06-09 20:38:09 +0800

    完善

--- util/aueryData.js
+++ util/aueryData.js
@@ -12,6 +12,7 @@ class AueryData {
      * @param {Object} -obj [需要获取的参数名]
      * @param {String} -obj [需要获取的参数名].type 获取参数类型 默认String  可选"String"/'Object'/'Number'/'Boolean'/'Array'
      * @param {Array} -obj [需要获取的参数名].typeList 获取参数类型 默认String  可选"String"/'Object'/'Number'/'Boolean'/'Array'
+     * @param {Boolean} -obj [需要获取的参数名].dataMust true/false, 对象和数组，能否为空对象或者空数组
      * @param {Boolean} -obj [需要获取的参数名].must true/false, 是否是必须获取的
      * @param {Object} -obj [需要获取的参数名].keyList 对象里面的key列表,就像外层一样。
      * @param {Object} req request
@@ -62,6 +63,36 @@ class AueryData {
                     // 判断参数是否有误，有误则返回错误信息，无误则将信息返回                    
                     if (v != undefined && v != '' && v != null && v != "undefined" && v != "null") {
                         if (typeof v == element.type) {
+                            switch (element.type) {
+                                case 'string':
+                                    if (v == ' ') {
+                                        return res.json(new Code({
+                                            code: 1002,
+                                            msg: '参数有误,' + item + ',传入参数为空字符串',
+                                        }).return);
+                                    }
+                                    break;
+                                case 'object':
+                                    if (element.dataMust) {
+                                        if (JSON.stringify(v) == '{}') {
+                                            return res.json(new Code({
+                                                code: 1002,
+                                                msg: '参数有误,' + item + ',传入参数为空对象',
+                                            }).return);
+                                        }
+                                    }
+                                    break;
+                                case 'array':
+                                    if (element.dataMust) {
+                                        if (JSON.stringify(v) == '[]') {
+                                            return res.json(new Code({
+                                                code: 1002,
+                                                msg: '参数有误,' + item + ',传入参数为空数组',
+                                            }).return);
+                                        }
+                                    }
+                                    break;
+                            }
                             R[item] = v;
                             // 这里判断传递值可以是多种数据类型
                         } else if (element.typeList && typeof element.typeList == 'array' && element.typeList.length) {
@@ -72,12 +103,74 @@ class AueryData {
                                 }
                             });
                             if (et) {
-                                R[item] = v;
+                                // 类型强制转换
+                                let sv = '';
+                                switch (element.type) {
+                                    case 'string':
+                                        if (String(v) == 'null' || String(v) == 'undefined') {
+                                            // throw new Error('参数强制转换string类型转换失败，获取值为 null or undefined');
+                                            return res.json(new Code({
+                                                code: 1002,
+                                                msg: '参数有误,' + item + ',参数强制转换string类型转换失败,获取值为 null or undefined ' + v,
+                                            }).return);
+                                        } else {
+                                            sv = String(v);
+                                        }
+                                        break;
+                                    case 'object':
+                                        if (AueryData.isJSON(v)) {
+                                            if (typeof JSON.parse(v) == 'object') {
+                                                sv = JSON.parse(v);
+                                            } else {
+                                                return res.json(new Code({
+                                                    code: 1002,
+                                                    msg: '参数有误,' + item + ',转换json数据类型不为object，数据类型为 ' + (typeof v),
+                                                }).return);
+                                            }
+                                        } else {
+                                            return res.json(new Code({
+                                                code: 1002,
+                                                msg: '参数有误,' + item + ',json转换失败，获取值为 ' + v,
+                                            }).return);
+                                        }
+                                        break;
+                                    case 'number':
+                                        if (isNaN(Number(v))) {
+                                            sv = Number(v);
+                                        } else {
+                                            return res.json(new Code({
+                                                code: 1002,
+                                                msg: '参数有误,' + item + ',参数强制转换number类型转换失败，获取值为 ' + v,
+                                            }).return);
+                                        }
+                                        break;
+                                    case 'boolean':
+                                        sv = Boolean(v);
+                                        break;
+                                    case 'array':
+                                        if (AueryData.isJSON(v)) {
+                                            if (typeof JSON.parse(v) == 'array') {
+                                                sv = JSON.parse(v);
+                                            } else {
+                                                return res.json(new Code({
+                                                    code: 1002,
+                                                    msg: '参数有误,' + item + ',转换json数据类型不为array，数据类型为 ' + (typeof v),
+                                                }).return);
+                                            }
+                                        } else {
+                                            return res.json(new Code({
+                                                code: 1002,
+                                                msg: '参数有误,' + item + ',json转换失败，获取值为 ' + v,
+                                            }).return);
+                                        }
+                                        break;
+                                }
+                                R[item] = sv;
                             } else {
                                 let str = element.typeList.join("，");
                                 str = str.substring(0, str.length - 1);
                                 return res.json(new Code({
-                                    code: 1001,
+                                    code: 1002,
                                     msg: '参数有误,' + item + ',数据格式：' + element.type + ',同时支持数据格式有：' + str,
                                 }).return);
                             }
@@ -88,7 +181,7 @@ class AueryData {
                             }).return);
                         } else {
                             return res.json(new Code({
-                                code: 1001,
+                                code: 1002,
                                 msg: '参数有误,' + item + '传递值为' + v + ',后端接收数据格式：' + element.type,
                             }).return);
                             t = false;
@@ -96,7 +189,7 @@ class AueryData {
                         }
                     } else if (element.must) {
                         return res.json(new Code({
-                            code: 1001,
+                            code: 1002,
                             msg: '缺少参数,' + item + ',数据格式：' + element.type,
                         }).return);
                         t = false;
@@ -104,6 +197,80 @@ class AueryData {
                     } else {
                         v = undefined;
                         R[item] = v;
+                    }
+                    // 
+                    if (element.keyList) {
+                        if (element.keyList) {
+                            if (typeof element.keyList == 'string') {
+                                R[item].forEach(i => {
+                                    if (typeof i != element.keyList) {
+                                        switch (element.type) {
+                                            case 'string':
+                                                if (String(v) == 'null' || String(v) == 'undefined') {
+                                                    // throw new Error('参数强制转换string类型转换失败，获取值为 null or undefined');
+                                                    return res.json(new Code({
+                                                        code: 1002,
+                                                        msg: '参数有误,' + item + ',参数强制转换string类型转换失败,获取值为 null or undefined ' + v,
+                                                    }).return);
+                                                } else {
+                                                    sv = String(v);
+                                                }
+                                                break;
+                                            case 'object':
+                                                if (AueryData.isJSON(v)) {
+                                                    if (typeof JSON.parse(v) == 'object') {
+                                                        sv = JSON.parse(v);
+                                                    } else {
+                                                        return res.json(new Code({
+                                                            code: 1002,
+                                                            msg: '参数有误,' + item + ',转换json数据类型不为object，数据类型为 ' + (typeof v),
+                                                        }).return);
+                                                    }
+                                                } else {
+                                                    return res.json(new Code({
+                                                        code: 1002,
+                                                        msg: '参数有误,' + item + ',json转换失败，获取值为 ' + v,
+                                                    }).return);
+                                                }
+                                                break;
+                                            case 'number':
+                                                if (isNaN(Number(v))) {
+                                                    sv = Number(v);
+                                                } else {
+                                                    return res.json(new Code({
+                                                        code: 1002,
+                                                        msg: '参数有误,' + item + ',参数强制转换number类型转换失败，获取值为 ' + v,
+                                                    }).return);
+                                                }
+                                                break;
+                                            case 'boolean':
+                                                sv = Boolean(v);
+                                                break;
+                                            case 'array':
+                                                if (AueryData.isJSON(v)) {
+                                                    if (typeof JSON.parse(v) == 'array') {
+                                                        sv = JSON.parse(v);
+                                                    } else {
+                                                        return res.json(new Code({
+                                                            code: 1002,
+                                                            msg: '参数有误,' + item + ',转换json数据类型不为array，数据类型为 ' + (typeof v),
+                                                        }).return);
+                                                    }
+                                                } else {
+                                                    return res.json(new Code({
+                                                        code: 1002,
+                                                        msg: '参数有误,' + item + ',json转换失败，获取值为 ' + v,
+                                                    }).return);
+                                                }
+                                                break;
+                                        }
+                                    }
+                                })
+                            }
+                            if (typeof element.keyList == 'object') {
+
+                            }
+                        }
                     }
                 } catch (error) {
                     console.error(error);
@@ -115,6 +282,34 @@ class AueryData {
                 }
             }
         }
+        return R[item];
+
+    }
+
+    static isJSON(str, pass_object) {
+        if (pass_object && isObject(str)) return true;
+
+        if (!isString(str)) return false;
+
+        str = str.replace(/\s/g, '').replace(/\n|\r/, '');
+
+        if (/^\{(.*?)\}$/.test(str))
+            return /"(.*?)":(.*?)/g.test(str);
+
+        if (/^\[(.*?)\]$/.test(str)) {
+            return str.replace(/^\[/, '')
+                .replace(/\]$/, '')
+                .replace(/},{/g, '}\n{')
+                .split(/\n/)
+                .map(function (s) {
+                    return isJSON(s);
+                })
+                .reduce(function (prev, curr) {
+                    return !!curr;
+                });
+        }
+
+        return false;
     }
 }
 
